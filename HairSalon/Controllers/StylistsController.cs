7d813bd1a9e9954ff79d997f0d387b95c1bce57e
Josh Tillinghast <jwtill@icloud.com>
2022-03-11 14:48:14 -0800

    Add Get and Post delete routes and views

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -40,6 +40,19 @@ namespace HairSalon.Controllers
       Stylist thisStylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == id);
       return View(thisStylist);
     }
+    public ActionResult Delete(int id)
+    {
+      var thisStylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == id);
+      return View(thisStylist);
+    }
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisStylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == id);
+      _db.Stylists.Remove(thisStylist);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
 
 
   }
